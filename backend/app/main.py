48a3cc2204e8c1e721f9b0dafd7a0cf709e23ad0
Conflict Zero Deploy <deploy@conflictzero.com>
2026-03-28 04:14:44 +0800

    Force deploy - LegalBot V3 fecha fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -145,3 +145,4 @@ if __name__ == "__main__":
 # LegalBot V3.0 Redeploy Sat Mar 28 03:55:04 AM CST 2026
 # Redeploy Sat Mar 28 03:57:58 AM CST 2026
 # Redeploy Sat Mar 28 04:05:24 AM CST 2026 - Force build
+# Force deploy Sat Mar 28 04:14:44 AM CST 2026
