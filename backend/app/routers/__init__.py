bcdbe47256f5d80a2a8b6827491d6a44bce54a14
Chuos24 <129564525+Chuos24@users.noreply.github.com>
2026-04-02 01:13:22 +0800

    FEAT: Export notifications_router

--- backend/app/routers/__init__.py
+++ backend/app/routers/__init__.py
@@ -6,5 +6,7 @@ from app.routers.consulta import router as consulta_router
 from app.routers.debug import router as debug_router
 from app.routers.compare import router as compare_router
 from app.routers.payments import router as payments_router
+from app.routers.admin import router as admin_router
+from app.routers.notifications import router as notifications_router
 
-__all__ = ["auth_router", "verification_router", "dashboard_router", "health_router", "consulta_router", "debug_router", "compare_router", "payments_router"]
+__all__ = ["auth_router", "verification_router", "dashboard_router", "health_router", "consulta_router", "debug_router", "compare_router", "payments_router", "admin_router", "notifications_router"]
